3c76a07a3538738061d55f447f1fa8b0a74600b7
nagarajmr <nagaraj.mr27@yahoo.co.in>
2018-06-07 19:53:10 +0100

    nmr: added code to update values

--- repository/UserRepository.js
+++ repository/UserRepository.js
@@ -107,7 +107,7 @@ var deleteUserByOID = (oid, callback) => {
     })
 };
 
-var test = (callback) => {
+var deleteOne = (callback) => {
     MongoClient.connect(url, (err, client) => {
         try {
             if (err) {
@@ -116,7 +116,7 @@ var test = (callback) => {
             else {
                 const db = client.db(users_database);
                 db.collection(users_collection)
-                    .deleteOne({name : 'Nagaraj M R'})
+                    .deleteOne({name: 'Nagaraj M R'})
                     .then((result) => {
                         console.log(result.result);
                     })
@@ -129,7 +129,62 @@ var test = (callback) => {
     })
 };
 
-test();
+var findAndDeleteOne = (oid, callback) => {
+    MongoClient.connect(url, (err, client) => {
+        try {
+            if (err) {
+                return console.log("Unable to connect to mongo db");
+            }
+            else {
+                const db = client.db(users_database);
+                db.collection(users_collection)
+                    .findOneAndDelete({_id: new ObjectID(oid)})
+                    .then((result) => {
+                        console.log(JSON.stringify(result, undefined, 2));
+                    })
+            }
+        }
+        finally {
+            console.log("Closing connection ...");
+            client.close();
+        }
+    })
+};
+
+var updateName = (oid, newName, callback) => {
+    MongoClient.connect(url, (err, client) => {
+        try {
+            if (err) {
+                return console.log("Unable to connect to mongo db");
+            }
+            else {
+                const db = client.db(users_database);
+                db.collection(users_collection)
+                    .findOneAndUpdate(
+                        {
+                            _id: new ObjectID(oid)
+                        },
+                        {
+                            $set : {
+                                name : newName
+                            }
+                        },
+                        {
+                            returnOriginal : false
+                        })
+                    .then((result) => {
+                        console.log(JSON.stringify(result, undefined, 2));
+                    })
+            }
+        }
+        finally {
+            console.log("Closing connection ...");
+            client.close();
+        }
+    })
+};
+
+updateName('5b0f15ed71c1310454756fb6', 'old name not back');
 
 module.exports = {
     save: saveRecord,
