ff2e9e27e0f09ceaa3d6bddf9a50be18faf213d8
nagarajmr <nagaraj.mr27@yahoo.co.in>
2018-06-14 23:31:06 +0100

    nmr: added get by object Id

--- controller/TodoController.js
+++ controller/TodoController.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 var {Todo} = require('../mongoose_module/model/Todo.js');
 var {User} = require('../mongoose_module/model/User.js');
 var {mongoose} = require('../mongoose_module/mongoose/MongooseDB.js');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 const port = 3000;
@@ -22,14 +23,14 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     var errorResponse = {
-        "err" : "Error occurred"
+        "err": "Error occurred"
     };
 
     Todo.find({}).then(
         (results) => {
             res.status(200).send({
-                "values" : results,
-                "count" : results.length
+                "values": results,
+                "count": results.length
             });
         },
         (error) => {
@@ -38,10 +39,35 @@ app.get('/todos', (req, res) => {
     )
 });
 
+app.get('/todos/:todoId', (req, res) => {
+    var todoId = req.params.todoId;
+    if (ObjectID.isValid(todoId)) {
+        Todo.findById(todoId)
+            .then((todo) => {
+                if(todo) {
+                    return res.status(200).send(todo);
+                }
+                else {
+                    return res.status(404).send({
+                        "message": "No results found"
+                    });
+                }
+            })
+            .catch((e) => {
+                return res.status(500).send(e);
+            });
+    }
+    else {
+        return res.status(400).send({
+            "message": "incorrect todo ID"
+        });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Started listening on ${port}`)
 });
 
 module.exports = {
-    app : app
+    app: app
 };
