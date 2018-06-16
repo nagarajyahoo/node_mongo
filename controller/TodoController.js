b241e770286668a61fdd13d5d81292dfd5800b56
nagarajmr <nagaraj.mr27@yahoo.co.in>
2018-06-16 17:54:58 +0100

    nmr: added put API to update todo

--- controller/TodoController.js
+++ controller/TodoController.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 var {Todo} = require('../mongoose_module/model/Todo.js');
@@ -9,6 +10,10 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+var errorResponse = {
+    "type": "error_response"
+};
 
 app.post('/todos', (req, res) => {
     var todo = new Todo(req.body);
@@ -69,7 +74,7 @@ app.delete('/todos/:id', (req, res) => {
     if (ObjectID.isValid(todoId)) {
         Todo.findByIdAndRemove(todoId)
             .then((deletedTodo) => {
-                if(deletedTodo) {
+                if (deletedTodo) {
                     return res.status(200).send(deletedTodo);
                 }
                 else {
@@ -93,6 +98,38 @@ app.delete('/todos/:id', (req, res) => {
             "type": "error_response",
             "message": "invalid todo ID"
         });
+    }
+});
+
+app.put('/todos/:id', (req, res) => {
+    var todoId = req.params.id;
+
+    if (ObjectID.isValid(todoId)) {
+        //this picks the text and completed variables from req
+        var body = _.pick(req.body, ['text', 'completed']);
+
+        if (_.isBoolean(body.completed) && body.completed) {
+            body.completedAt = new Date().getTime();
+        }
+
+        Todo.findByIdAndUpdate(todoId, {$set: body}, {new: true})
+            .then((updatedTodo) => {
+                if (updatedTodo) {
+                    return res.status(200).send(updatedTodo);
+                }
+                else {
+                    errorResponse.message = "Todo not found";
+                    return res.status(404).send(errorResponse);
+                }
+            })
+            .catch((ex) => {
+                errorResponse.message = "Unknown error occurred";
+                return res.status(500).send(errorResponse);
+            })
+    }
+    else {
+        errorResponse.message = "invalid todo ID";
+        return res.status(400).send(errorResponse);
     }
 });
 
